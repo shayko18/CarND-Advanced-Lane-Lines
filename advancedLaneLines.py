33815de68c40dac265c70cfc3adfed52d261f015
shayko18 <shaykoren18@gmail.com>
2017-03-29 17:56:58 +0300

    start with poly fit

--- advancedLaneLines.py
+++ advancedLaneLines.py
@@ -150,7 +150,7 @@ def sobel_binary_th(rgb_img, kernel_size=3, plot_en=False):
 	print('\t--> Start Sobel Binary Threshold')
 	# 1) convert to gray scale
 	gray_img = cv2.cvtColor(rgb_img, cv2.COLOR_RGB2GRAY)               # convert to gray scale
-	
+
 	# 2) Now we calculate all the sobels and sobel functions we need
 	sobel_x = cv2.Sobel(gray_img, cv2.CV_64F, 1, 0, ksize=kernel_size) # sobel on x-axis
 	sobel_y = cv2.Sobel(gray_img, cv2.CV_64F, 0, 1, ksize=kernel_size) # sobel on y-axis
@@ -287,11 +287,11 @@ def apply_binary_th(rgb_img, plot_en=False):
 	print('---> Start Binary Threshold')
 	b_sobel_undist_img = sobel_binary_th(rgb_img, kernel_size=3, plot_en=True) # b_ stands for binary  
 	b_color_undist_img = color_binary_th(rgb_img, plot_en=True) # b_ stands for binary
-	
+
 	# combine all the binary images
 	b_total = np.zeros_like(b_sobel_undist_img)
 	b_total[(b_sobel_undist_img == 1) | (b_color_undist_img == 1)] = 1
-	
+
 	if plot_en: # plot the binary image and the colored binary components
 		color_binary = np.dstack((b_sobel_undist_img, np.zeros_like(b_total),b_color_undist_img)) 
 		plt.figure(figsize=(16,8))
@@ -383,19 +383,126 @@ def find_birdeye_matrix(M_op=0, plot_en=False, mtx=None, dist=None):
 		M_ret, m_inv_ret = M[M_op-1], M_inv[M_op-1]
 	
 	return M_ret, m_inv_ret
+
+	
+### fit_lane_line: fit a polynomial to the binary birdeye image we get
+###                      Assume here this is the first image we have
+###   Input: 
+###		bird_b_undist_img: yhe input image
+###		plot_en: plot the original image and the fit
+###
+###   Output: 
+###       TODO	
+def fit_lane_line(bird_b_undist_img, plot_en=False):
+	print('---> Start Fit Polynomial')
+	
+	# 1) we find the starting point of the lanes. We also reate an output image to draw on and  visualize the result
+	histogram = np.sum(bird_b_undist_img[bird_b_undist_img.shape[0]>>1:,:], axis=0)    # Take a histogram of the bottom half of the image
+	out_img = np.dstack((bird_b_undist_img, bird_b_undist_img, bird_b_undist_img))*255 # Create an output image to draw on and visualize the result
+	midpoint = np.int(histogram.shape[0]>>1)
+	leftx_base = np.argmax(histogram[:midpoint])              # Find the peak of the left half of the histogram
+	rightx_base = np.argmax(histogram[midpoint:]) + midpoint  # Find the peak of the left half of the histogram
+
+	# 2) define the sliding window and initialize two vectors (x,y) with all the nonzeros values
+	nwindows = 9                                                # number of sliding windows
+	window_height = np.int(bird_b_undist_img.shape[0]/nwindows) # Set height of windows
+	nonzero = bird_b_undist_img.nonzero()                       # Identify the x and y positions of all nonzero pixels in the image
+	nonzeroy = np.array(nonzero[0])
+	nonzerox = np.array(nonzero[1])
+	
+	
+	# 3) We initialze the strating positions and global values for our scanning 
+	margin = 100                   # Set the width of the windows +/- margin
+	minpix = 50                    # Set minimum number of pixels found to recenter window
+	left_lane_inds = []            # Create empty lists to receive left lane pixel indices
+	right_lane_inds = []           # Create empty lists to receive right lane pixel indices
+	leftx_current = leftx_base     # Current positions to be updated for each window. left lane
+	rightx_current = rightx_base   # Current positions to be updated for each window. right lane
+	
+	# 4) we start step through the windows one by one
+	for window in range(nwindows):
+		# Identify window boundaries in x and y (and right and left)
+		win_y_low = bird_b_undist_img.shape[0] - (window+1)*window_height
+		win_y_high = bird_b_undist_img.shape[0] - window*window_height
+		win_xleft_low = leftx_current - margin
+		win_xleft_high = leftx_current + margin
+		win_xright_low = rightx_current - margin
+		win_xright_high = rightx_current + margin
+		
+		# Draw the windows on the visualization image
+		cv2.rectangle(out_img,(win_xleft_low,win_y_low),(win_xleft_high,win_y_high),(0,255,0), 2) 
+		cv2.rectangle(out_img,(win_xright_low,win_y_low),(win_xright_high,win_y_high),(0,255,0), 2) 
+		
+		# Identify the nonzero pixels in x and y within the window
+		good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & (nonzerox >= win_xleft_low) & (nonzerox < win_xleft_high)).nonzero()[0]
+		good_right_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & (nonzerox >= win_xright_low) & (nonzerox < win_xright_high)).nonzero()[0]
+		# Append these indices to the lists
+		left_lane_inds.append(good_left_inds)
+		right_lane_inds.append(good_right_inds)
+		# If you found > minpix pixels, recenter next window on their mean position
+		if len(good_left_inds) > minpix:
+			leftx_current = np.int(np.mean(nonzerox[good_left_inds]))
+		if len(good_right_inds) > minpix:        
+			rightx_current = np.int(np.mean(nonzerox[good_right_inds]))
+
+	# Concatenate the arrays of indices
+	left_lane_inds = np.concatenate(left_lane_inds)
+	right_lane_inds = np.concatenate(right_lane_inds)
+
+	# Extract left and right line pixel positions
+	leftx = nonzerox[left_lane_inds]
+	lefty = nonzeroy[left_lane_inds] 
+	rightx = nonzerox[right_lane_inds]
+	righty = nonzeroy[right_lane_inds] 
+
+	# Fit a second order polynomial to each
+	left_fit = np.polyfit(lefty, leftx, 2)
+	right_fit = np.polyfit(righty, rightx, 2)
+
+	
+	if plot_en:
+		plt.figure(figsize=(16,8))
+		# Generate x and y values for plotting
+		ploty = np.linspace(0, bird_b_undist_img.shape[0]-1, bird_b_undist_img.shape[0] )
+		left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
+		right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
+
+		out_img[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [255, 0, 0]
+		out_img[nonzeroy[right_lane_inds], nonzerox[right_lane_inds]] = [0, 0, 255]
+		plt.imshow(out_img)
+		plt.plot(left_fitx, ploty, color='yellow')
+		plt.plot(right_fitx, ploty, color='yellow')
+		plt.xlim(0, 1280)
+		plt.ylim(720, 0)
+		
+		plt.savefig('output_images/fit_polynomial.png')
+	
 	
 ####  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Part B: Main Pipeline ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  ####
 
 ###
-### We will find the calibration parameters, and test it one the test image
+### Stage 1: We will find the calibration parameters, and test it one the test image
+### 		 We will plot an example on the chessboard
 ret, mtx, dist, rvecs, tvecs = find_calibration_params(nx=9, ny=6, dn=3, plot_en=True)	# find Calibration parameters
-rgb_undist_img = calibrate_road_image(mtx, dist, idx=0, fname='straight_lines', plot_en=True) # apply the Calibration parameters on one of the test images
+rgb_undist_img = calibrate_road_image(mtx, dist, idx=0, fname='test', plot_en=True) # apply the Calibration parameters on one of the test images
 
 ###
-### Next we will try to identify the lane lines on the undistorted image using:
-###       - sobel (gradient vector)
-###       - thresholding in different color spaces 
-### We will binary slice the different outputs and finally combine the two (OR operator on the overlapping regions)
-### We will plot an example on what we got on a random road test image (the same image we got before)
+### Stage 2: We will try to identify the lane lines on the undistorted image using:
+###      		- sobel (gradient vector)
+###       		- thresholding in different color spaces 
+### 		We will binary slice the different outputs and finally combine the two (OR operator on the overlapping regions)
+### 		We will plot an example on what we got on a random road test image (the same image we got before)
 b_undist_img = apply_binary_th(rgb_undist_img, plot_en=True) # b_ stands for binary  
-M, M_inv = find_birdeye_matrix(M_op=1, plot_en=True, mtx=mtx, dist=dist)
+
+
+###
+### Stage 3: We will calculate the birdeye transform matrix (and its inverse):
+### 		 We will plot an example on the straight_lines images
+M, M_inv = find_birdeye_matrix(M_op=1, plot_en=True, mtx=mtx, dist=dist)                                       # calc the matrix
+bird_b_undist_img = cv2.warpPerspective(b_undist_img, M, (rgb_undist_img.shape[1], rgb_undist_img.shape[0])) # apply the matrix
+
+###
+### Stage 4: We will fit a polynomial to the lane lines we found.
+###          Assume here this is the first image we have
+### 		 We will plot an example of it
+fit_lane_line(bird_b_undist_img, plot_en=True)
